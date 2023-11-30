a28229161c009def0039f7f8509b6f855a7ff9b6
genesysrm <genesys.merchan@gmail.com>
2023-11-30 16:23:33 -0300

    refactoracion

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,6 @@
 package com.aluracursos.screenmatch;
 
-import com.aluracursos.screenmatch.principal.Principal;
+import com.aluracursos.screenmatch.pruebas.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,8 +14,18 @@ public class ScreenmatchApplication  implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Principal principal = new Principal();
-		principal.muestraElMenu();
+//		Principal principal = new Principal();
+//		principal.muestraElMenu();
+//		var buscarEpisodios = new BuscarEpisodios();
+//		buscarEpisodios.buscarEpisodios();
+//		var ejemploStreams = new EjemploStreams();
+//		ejemploStreams.muestraListaDeInstructores();
+//		var topEpisodios = new TopEpisodios();
+//		topEpisodios.muestraTopEpisodios();
+//		var buscarEpisodio = new BuscarEpisodiosPorParametros();
+//		buscarEpisodio.buscarEpisodiosPorParametro();
+		var estadisticas = new TrabajarEstadisticas();
+		estadisticas.mostrarEstadisticas();
 
 
 
---
+++ src/main/java/com/aluracursos/screenmatch/pruebas/EjemploStreams.java
@@ -0,0 +1,18 @@
+package com.aluracursos.screenmatch.pruebas;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class EjemploStreams {
+    public void muestraListaDeInstructores(){
+        List<String> nombres = Arrays.asList("Genesys","Eric","Maria","Brenda");
+
+        nombres.stream()
+                .sorted()
+                .limit(2)
+                .filter(n -> n.startsWith("E"))
+                .map(n -> n.toUpperCase())
+                .forEach(System.out::println);
+    }
+
+}
